6810384eceff173a41bf70a4d2241f9634b02598
Huy Tran Tuan <tth@aod.vn>
2025-11-10 17:22:31 +0700

    add resident

---
+++ ApartmentManagementSystem.EF/Context/ApartmentMember.cs
@@ -0,0 +1,14 @@
+﻿using ApartmentManagementSystem.EF.Context.Base;
+
+
+namespace ApartmentManagementSystem.EF.Context
+{
+    public class ApartmentMember : AuditEntity<Guid>
+    {
+        public Guid ApartmentId { get; set; }
+        public Apartment Apartment { get; set; }
+        public Guid ResidentId { get; set; }
+        public Resident Resident { get; set; }
+        public string MemberType { get; set;}
+    }
+}
---
+++ ApartmentManagementSystem.EF/Context/Resident.cs
@@ -0,0 +1,15 @@
+﻿using ApartmentManagementSystem.EF.Context.Base;
+
+
+namespace ApartmentManagementSystem.EF.Context
+{
+    public class Resident : AuditEntity<Guid>
+    {
+        public Guid ApartmentBuildingId { get; set; }
+        public ApartmentBuilding ApartmentBuilding { get; set; }
+        public string Name { get; set; }
+        public DateTime? BrithDay { get; set; }
+        public string? IdentityNumber { get; set; }
+        public string? UserId { get; set; }
+    }
+}
